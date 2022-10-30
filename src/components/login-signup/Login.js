b4ca90202329865dfdb4d80e51d891e78865a2df
akbloodadarsh <adarsh.infinite@gmail.com>
2022-10-30 22:00:40 +0530

    added cookie for storing token

--- src/components/login-signup/Login.js
+++ src/components/login-signup/Login.js
@@ -1,19 +1,16 @@
 import React from 'react'
-import Typography from '@mui/material/Typography'
-import Slider from '@mui/material/Slider';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Cookies from 'universal-cookie';
 
 export default function Login() {
     const [values, setValues] = React.useState({
         username: '',
         password: ''
       });
+    
+    const cookies = new Cookies();
 
     const handleChange = event => {
         setValues({ ...values, [event.target.name]: event.target.value });
@@ -25,18 +22,8 @@ export default function Login() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(
                 {
-                    "user_id": "",
                     "user_name": values.username,
-                    "first_name": "",
-                    "middle_name": "",
-                    "last_name": "",
                     "password": values.password,
-                    "date_of_birth": "",
-                    "gender": "",
-                    "skill": [],
-                    "profile_pic": "",
-                    "gmail": "",
-                    "resume": ""
                 }
             )
         };
@@ -44,7 +31,8 @@ export default function Login() {
         await fetch(process.env.REACT_APP_API + "api/Users/authenticate-user", requestOptions)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data.token)
+            cookies.set('token', data.token, { path: '/' });
+            console.log(cookies.get('token'))
         });
     }
 
